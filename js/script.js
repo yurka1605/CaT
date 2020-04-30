482a077d56c9e557bfc4fddc45f3540915424d1f
Litvinov Yuriy <u.litvinov@elewise.com>
2020-04-30 22:49:17 +0400

    init(main): search-block

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
+
+// import Inputmask from 'inputmask';
+
 window.onload = function() {
-    
+    document.querySelectorAll('.sort-item').forEach(item => {
+        item.addEventListener('click', function () {
+            this.classList.toggle('active'); 
+        });
+    });
+    document.querySelectorAll('.drop').forEach(item => {
+        item.addEventListener('click', function () {
+            this.classList.toggle('active');
+        })
+    });
+    const daterange = document.querySelector('.daterange input');
+
+    // var im = new Inputmask("31.12.2099 - 31.12.2099");
+    // im.mask(daterange);
 };
 
 /** ======================== User actions ========================== **/
